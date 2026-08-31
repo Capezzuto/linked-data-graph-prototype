591800b1283acf3222bfa83de9c4428d1ddc1dc8
Joe Capezzuto <joseph.capezzuto@gmail.com>
2026-08-31 12:02:01 -0700

    Add svg to page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import * as d3 from 'd3';
 
+const width = Math.min(500, window.screen.width - 120);
+const height = Math.min(500, window.screen.height - 120);
+
 const container = document.getElementById('app');
 console.log('container', container);
+
+const svg = d3
+  .create('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .attr('viewbox', `0 0 ${width} ${height}`)
+  .style('background', '#333333');
+
+container.append(svg.node());
